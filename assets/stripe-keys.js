a25a145d96369c5ede934938ec111bbb59ee1ecb
JereUR <jereur.jdv@gmail.com>
2022-06-29 02:59:58 -0300

    Stripe Testing

--- assets/stripe-keys.js
+++ assets/stripe-keys.js
@@ -1,6 +1,6 @@
 export default {
   public:
-    "pk_test_51LFmo0Ltl671L1L1EcH986onBdYGT2qc6JiJaML27klorU5z9jyYLF8Uz9iFcvFYSUp1p9wFmaLxfyPNs1Gs8xmW00AKOyyjBs",
+    "Your Public Stripe Key",
   secret:
-    "sk_test_51LFmo0Ltl671L1L1049m2TBt8dVfjZSMASp4vGzHhFTD1Xq8QTXM1GDsB8xsRM76NNUniwNwHU5ZYpc8gAPSlhZo00po08CLpg",
+    "Your Secret Stripe Key",
 };
